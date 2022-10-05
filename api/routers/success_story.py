d1de54c212f62d1f27297a4ef743652bae6ce201
Kate <katej61487@gmail.com>
2022-10-05 17:13:25 -0400

    created Create Success Story

---
+++ api/routers/success_story.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Depends, HTTPException
+from models import SuccessStoryIn, SuccessStoryList, SuccessStoryOut
+from queries.success_story import SuccessStoryQueries
+
+router = APIRouter()
+
+@router.post('/api/pets/{id}/story/')
+def create_story (
+    story:SuccessStoryIn, 
+    queries:SuccessStoryQueries=Depends()):
+    response = queries.create_story(story)
+    return response
