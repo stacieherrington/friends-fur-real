70077ba2a49677979ea447fc6d3d0747cac812da
Tyler Male <postino.amichevole@gmail.com>
2022-10-11 19:54:37 -0400

    changed main.py to not display too much in FastAPI page

--- api/main.py
+++ api/main.py
@@ -1,33 +1,30 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
-from routers import pet, adoption_application, rescue, success_story, accounts, auth
+from routers import (
+    pet,
+    adoption_application,
+    rescue,
+    success_story,
+    accounts,
+    auth,
+)
 
 app = FastAPI(
-    # # renames fastapi website
-    # title="Friends Fur Real - Routers",
-    # # description for fastapi
-    # description="Your destination for testing routers for each of the following models: "
-    # " Accounts, Pets, Adoption Applications and Success Stories"
-    # "  ©"
-    # "Pawsibilities 2022",
-    # # terms of service?
-    # terms_of_service="http://example.com/terms/",
-    # # display version in grey bubble
-    # version="16.4.0.2",
-    # # sets the /route
-    # docs_url="/pawsible",
-    # # contact info
-    # contact={
-    #     "name": "Friends Fur Real",
-    #     "url": "http://localhost:3000",
-    #     "email": "dp@x-force.example.com",
-    # },
-    # # license info
-    # license_info={
-    #     "name": "©Pawsibilities2022",
-    #     "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
-    # },
+    title="Friends Fur Real - Routers",
+    description="Your destination for testing routers for each of the following models: "
+    " Accounts, Pets, Adoption Applications and Success Stories"
+    "  ©"
+    "Pawsibilities 2022",
+    version="16.4.0.2",
+    docs_url="/pawsible",
+    contact={
+        "name": "Friends Fur Real",
+        "url": "http://localhost:3000",
+    },
+    license_info={
+        "name": "©Pawsibilities2022",
+    },
 )
 
 app.add_middleware(
