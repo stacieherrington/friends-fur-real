77ca6234faa187cf159ad9a3b1cc6d2a32f65992
Kate <katej61487@gmail.com>
2022-10-12 15:45:09 -0400

    created navbar template

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,38 +1,22 @@
-import { useEffect, useState } from 'react';
-import Construct from './Construct.js'
-import ErrorNotification from './ErrorNotification';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import HomePage from './Homepage';
+import NavBar from './nav';
 
 function App() {
-  const [launch_info, setLaunchInfo] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_API_HOST}/api/launch-details`;
-      console.log('fastapi url: ', url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
-
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
-    }
-    getData();
-  }, [])
-
 
   return (
-    <div>
-      <h1>hello world!</h1>
-      <ErrorNotification error={error} />
-      <Construct info={launch_info} />
-    </div>
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ ghi/src/nav.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+
+function NavBar() {
+  return (
+    <>
+      <div className="container-fluid">
+        <Navbar bg="light" variant="light" expand="lg">
+          <Container>
+            <div>
+              <Navbar.Brand href="#home">
+                <img
+                  src="images/cute-icon.png"
+                  width="100"
+                  height="100"
+                  className="d-inline-block align-top"
+                  alt="React Bootstrap logo"
+                />
+              </Navbar.Brand>
+            </div>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <NavDropdown title="Rescues" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Link 1</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.2">
+                    Link 2
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.3">Link 3</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Link 4
+                  </NavDropdown.Item>
+                </NavDropdown>
+
+                <NavDropdown title="Adopters" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Link 1</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.2">
+                    Link 2
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.3">Link 3</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Link 4
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+
+              <Nav>
+                <Nav.Link href="#signup">
+                  <Button variant="outline-secondary">Signup</Button>
+                </Nav.Link>
+                <Nav.Link eventKey={2} href="#login">
+                  <Button variant="outline-secondary">Login</Button>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    </>
+  )
+}
+
+export default NavBar;
