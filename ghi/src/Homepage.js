61daffb8f1b9851cf00d4ad0cf5c7e182328cf08
Kate <katej61487@gmail.com>
2022-10-13 16:44:07 -0400

    added search component

--- ghi/src/Homepage.js
+++ ghi/src/Homepage.js
@@ -1,16 +1,14 @@
-import React from 'react';
+import * as React from 'react';
+import SearchBar from './components/SearchBar';
+
 
 function HomePage() {
   return (
-    <div className="px-4 py-5 my-5 text-center">
-      <h1 className="display-5 fw-bold">FriendsFurReal</h1>
-      <div className="col-lg-6 mx-auto">
-        <p className="lead mb-4">
-          TEST!
-        </p>
-      </div>
+    <div className="px-4 my-5 text-center">
+      <SearchBar />
     </div>
   )
 }
+
 
 export default HomePage;
