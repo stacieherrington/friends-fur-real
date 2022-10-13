2f715fc7a2cd1f3f9185c5c39f22c87e5fa7c842
Stacie <stacieherrington@gmail.com>
2022-10-13 15:30:22 -0500

    Added update and delete endpoints for Success Stories.

--- api/routers/success_story.py
+++ api/routers/success_story.py
@@ -34,6 +34,25 @@ def list_rescue_stories(
     response = queries.get_stories_by_rescue(rescue_id)
     return SuccessStoryList(stories=response)
 
+
 @router.get("/api/stories/random", response_model=SuccessStoryList)
 def get_three_random_stories(queries: SuccessStoryQueries = Depends()):
     return SuccessStoryList(stories=queries.get_three_random_stories())
+
+
+@router.delete("/api/stories/{id}/")
+def delete_pet(id:str,queries: SuccessStoryQueries=Depends()):
+    response = queries.delete_story(id)
+    if response:
+        return response
+    else:
+        raise HTTPException(404, "This story id does not exist!")
+
+
+@router.put("/api/stories/{id}/", response_model=SuccessStoryOut)
+def update_story(id:str, data: SuccessStoryIn, queries: SuccessStoryQueries=Depends()):
+    response = queries.update_story(id, data)
+    if response:
+        return response
+    else:
+        raise HTTPException(404, "This story id does not exist!")
