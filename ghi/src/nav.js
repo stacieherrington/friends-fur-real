50080b3aa89cca03851f99f64eac95d82b60facd
Tyler Male <postino.amichevole@gmail.com>
2022-10-14 15:34:39 -0400

    might have resolved conflicts?

--- ghi/src/nav.js
+++ ghi/src/nav.js
@@ -1,74 +1,114 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Button from 'react-bootstrap/Button';
+import { NavLink, useNavigate } from "react-router-dom";
+import { useGetTokenQuery, useLogoutMutation } from "./endpoints/authEndpoints";
+import { useDispatch } from "react-redux";
+import {
+  showModal,
+  LOGIN_MODAL,
+  SIGNUP_MODAL,
+} from "./endpoints/accountSlice";
+import logo from "./logo.svg";
+import LoginModal from "./modals/loginModal";
+import SignupModal from "./modals/signupModal";
+import { useEffect } from "react";
 
-function NavBar() {
+function LoginButtons(props) {
+  const dispatch = useDispatch();
+  const classNames = `buttons ${props.show ? "" : "is-hidden"}`;
+
   return (
-    <>
-      <div className="container-fluid mt-5">
-        <Navbar bg="light" variant="light" expand="lg">
-          <Container>
-            <div>
-              <Navbar.Brand href="#home">
-                <img
-                  src="images/cute-icon.png"
-                  width="100"
-                  height="100"
-                  className="d-inline-block align-top"
-                  alt="React Bootstrap logo"
-                />
-              </Navbar.Brand>
-            </div>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <NavDropdown title="Rescues" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Link 1</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.2">
-                    Link 2
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.3">Link 3</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Link 4
-                  </NavDropdown.Item>
-                </NavDropdown>
-
-                <NavDropdown title="Adopters" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Link 1</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.2">
-                    Link 2
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.3">Link 3</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Link 4
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-
-              <Nav>
-                <Nav.Link href="#signup">
-                  <Button variant="outline-secondary">Signup</Button>
-                </Nav.Link>
-                <Nav.Link eventKey={2} href="#login">
-                  <Button variant="outline-secondary">Login</Button>
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </div>
-    </>
-  )
+    <div className={classNames}>
+      <button
+        onClick={() => dispatch(showModal(SIGNUP_MODAL))}
+        className='button is-primary'
+      >
+        <strong>Sign up</strong>
+      </button>
+      <button
+        onClick={() => dispatch(showModal(LOGIN_MODAL))}
+        className='button is-light'
+      >
+        Login
+      </button>
+    </div>
+  );
 }
 
-export default NavBar;
+function LogoutButton() {
+  const navigate = useNavigate();
+  const [logout, { data }] = useLogoutMutation();
+  useEffect(() => {
+    if (data) {
+      navigate("/");
+    }
+  }, [data, navigate]);
+  return (
+    <div className='buttons'>
+      <button onClick={logout} className='button is-light'>
+        Logout
+      </button>
+    </div>
+  );
+}
+
+function Nav() {
+  const { data: token, isLoading: tokenLoading } = useGetTokenQuery();
+  const {
+    account: { roles = [] },
+  } = token || { account: {} };
+  return (
+    <>
+      <nav
+        className='navbar is-link is-fixed-top'
+        role='navigation'
+        aria-label='main navigation'
+      >
+        <div className='navbar-brand'>
+          <NavLink className='navbar-item' to='/'>
+            <img src={logo} height='86' width='43' alt='' />
+          </NavLink>
+          <button
+            className='navbar-burger'
+            aria-label='menu'
+            aria-expanded='false'
+            data-target='navbarBasic'
+          >
+            <span aria-hidden='true'></span>
+            <span aria-hidden='true'></span>
+            <span aria-hidden='true'></span>
+          </button>
+        </div>
+        <div id='navbarBasic' className='navbar-menu'>
+          <div className='navbar-start'>
+            <NavLink className='navbar-item' to='/'>
+              Staff
+            </NavLink>
+            {roles.includes("staff") ? (
+              <>
+                <NavLink className='navbar-item' to='adoption_application/list'>
+                  Adoption Applications List
+                </NavLink>
+              </>
+            ) : (
+              ""
+            )}
+          </div>
+          <div className='navbar-end'>
+            <div className='navbar-item'>
+              {tokenLoading ? (
+                <LoginButtons show={false} />
+              ) : token ? (
+                <LogoutButton />
+              ) : (
+                <LoginButtons show={true} />
+              )}
+            </div>
+          </div>
+        </div>
+      </nav>
+      <LoginModal />
+      <SignupModal />
+    </>
+  );
+}
+
+export default Nav;
