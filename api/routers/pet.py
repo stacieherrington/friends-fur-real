98c15be9a25a73d5f608b1d926932a6ac9928d94
GaryTang <garytang412@gmail.com>
2022-10-14 16:41:02 -0500

    make url id more clear to display

--- api/routers/pet.py
+++ api/routers/pet.py
@@ -5,9 +5,9 @@ from queries.pet import PetQueries
 router = APIRouter(tags=["Pets"])
 
 
-@router.get("/api/pets/{id}/", response_model=PetOut)
-def get_pet(id: str, queries: PetQueries = Depends()):
-    response = queries.get_pet(id)
+@router.get("/api/pets/{pet_id}/", response_model=PetOut)
+def get_pet(pet_id: str, queries: PetQueries = Depends()):
+    response = queries.get_pet(pet_id)
     if response:
         return response
     else:
@@ -25,18 +25,18 @@ def list_pets(queries: PetQueries = Depends()):
     return PetsList(pets=queries.list_pets())
 
 
-@router.delete("/api/pets/{id}/")
-def delete_pet(id: str, queries: PetQueries = Depends()):
-    response = queries.delete_pet(id)
+@router.delete("/api/pets/{pet_id}/")
+def delete_pet(pet_id: str, queries: PetQueries = Depends()):
+    response = queries.delete_pet(pet_id)
     if response:
         return response
     else:
         raise HTTPException(404, "this pet id does not exist!")
 
 
-@router.put("/api/pets/{id}/", response_model=PetOut)
-def update_pet(id: str, data: PetIn, queries: PetQueries = Depends()):
-    response = queries.update_pet(id, data)
+@router.put("/api/pets/{pet_id}/", response_model=PetOut)
+def update_pet(pet_id: str, data: PetIn, queries: PetQueries = Depends()):
+    response = queries.update_pet(pet_id, data)
     if response:
         return response
     else:
