0f851909633c4d5c367e87589392d306f0d51a1b
Tyler Male <postino.amichevole@gmail.com>
2022-10-18 00:52:50 -0400

    application form design done, submit not handled, state not handled

--- ghi/src/applications/ApplicationForm.js
+++ ghi/src/applications/ApplicationForm.js
@@ -4,8 +4,7 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
+import MenuItem from "@mui/material/MenuItem";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
@@ -13,10 +12,8 @@ import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Copyright from "../components/Copyright";
 import { MuiTelInput } from "mui-tel-input";
-// import FormGroup from '@mui/material/FormGroup';
-// import FormControlLabel from '@mui/material/FormControlLabel';
-// import Checkbox from '@mui/material/Checkbox';
 import AppCheckbox from "./appCheckbox";
+import SendSharpIcon from '@mui/icons-material/SendSharp';
 
 function PhoneNumber() {
   const [phone, setPhone] = useState("");
@@ -29,21 +26,64 @@ function PhoneNumber() {
       value={phone}
       placeholder='Phone Number'
       onChange={handleChange}
-      onlyCountries={['US']}
+      onlyCountries={["US"]}
       fullWidth
     />
+  );
+}
+
+function ResidenceSelector() {
+  const residences = [
+    { value: "Single Family", label: "Single Family" },
+    { value: "Apartment", label: "Apartment" },
+    { value: "Townhouse", label: "Townhouse" },
+    { value: "Condo", label: "Condo" },
+  ];
+  const [residenceType, setResidenceType] = React.useState("");
+  const handleSelect = (event) => {
+    setResidenceType(event.target.value);
+  };
+  return (
+    <TextField
+      id='outlined-select-residence'
+      select
+      fullWidth
+      label='Residence Type'
+      value={residenceType}
+      onChange={handleSelect}
+    >
+      {residences.map((option) => (
+        <MenuItem key={option.value} value={option.value}>
+          {option.label}
+        </MenuItem>
+      ))}
+    </TextField>
   );
 }
 
 const theme = createTheme();
 
 export default function ApplicationForm() {
+  // const [first_name, setFirst_name] = useState("");
+  // const [last_name, setLast_name] = useState("");
+  // const [phoneNumber, setPhoneNumber] = useState("");
+  // const [date_ready, setDate_ready] = useState("");
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get("email"),
-      password: data.get("password"),
+      first_name: data.get("first_name"),
+      last_name: data.get("last_name"),
+      phone_number: data.get("phoneNumber"),
+      date_ready: data.get("date_ready"),
+      landlord_restrictions: data.get("landlord_restrictions"),
+      residence_type: data.get("residence_type"),
+      has_dogs: data.get("has_dogs"),
+      has_cats: data.get("has_cats"),
+      smoker: data.get("smoker"),
+      has_small_children: data.get("has_small_children"),
+      residence_owned: data.get("residence_owned"),
     });
   };
 
@@ -70,81 +110,55 @@ export default function ApplicationForm() {
               margin='normal'
               required
               fullWidth
-              id='email'
+              id='first_name'
               placeholder='First Name'
-              name='firstName'
-              autoComplete='current-firstname'
+              name='first_name'
+              autoComplete='current-first-name'
               autoFocus
             />
             <TextField
               margin='normal'
               required
               fullWidth
-              name='lastName'
+              name='last_name'
               placeholder='Last Name'
               type='text'
-              id='lastName'
-              autoComplete='current-lastname'
+              id='last_name'
+              autoComplete='current-last-name'
             />
             <PhoneNumber />
-            <AppCheckbox />
             <TextField
               margin='normal'
               required
               fullWidth
-              name='residence_type'
-              placeholder='Type of Residence'
-              type='text'
-              id='address'
-              autoComplete='current-address'
-            />
-
-            <TextField
-              margin='normal'
-              required
-              fullWidth
-              name='password'
-              placeholder='Residence Owned'
-              type='password'
-              id='password'
-              autoComplete='current-password'
-            />
-            <TextField
-              margin='normal'
-              required
-              fullWidth
-              name='password'
-              placeholder='Landlord Restrictions'
-              type='password'
-              id='password'
-              autoComplete='current-password'
-            />
-            <TextField
-              margin='normal'
-              required
-              fullWidth
-              name='password'
+              name='date_ready'
               placeholder='Date Ready'
               type='date'
-              id='password'
-              autoComplete='current-password'
+              id='date_ready'
+              autoComplete='current-date'
             />
             <TextField
               margin='normal'
               required
               fullWidth
-              name='password'
-              label='Password'
-              type='password'
-              id='password'
-              autoComplete='current-password'
+              multiline
+              maxRows={4}
+              name='landlord_restrictions'
+              placeholder='Landlord Restrictions'
+              type='text'
+              id='landlord_restrictions'
+              autoComplete='current-landlord-restrictions'
             />
+            <ResidenceSelector />
+            <AppCheckbox />
+
             <Button
               type='submit'
               fullWidth
-              variant='contained'
+              variant='outlined'
               sx={{ mt: 3, mb: 2 }}
               color='success'
+              endIcon={<SendSharpIcon/>}
             >
               Submit
             </Button>
--- ghi/src/applications/appCheckbox.js
+++ ghi/src/applications/appCheckbox.js
@@ -4,91 +4,140 @@ import FormLabel from "@mui/material/FormLabel";
 import FormControl from "@mui/material/FormControl";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import FormHelperText from "@mui/material/FormHelperText";
 import Checkbox from "@mui/material/Checkbox";
+import SmokeFreeSharpIcon from "@mui/icons-material/SmokeFreeSharp";
+import SmokingRoomsSharpIcon from "@mui/icons-material/SmokingRoomsSharp";
+import DoneOutlineSharpIcon from "@mui/icons-material/DoneOutlineSharp";
 
 export default function AppCheckbox() {
-  const [state, setState] = React.useState({
-    children: false,
-    dogs: false,
-    cats: false,
-    preApproval: false,
-    terms: false,
+  const [checkboxValues, setCheckboxValues] = React.useState({
+    has_small_children: false,
+    has_dogs: false,
+    has_cats: false,
+    wants_prepproval: false,
+    agrees_to_terms: false,
+    residence_owned: false,
+    smoker: true,
   });
 
   const handleChange = (event) => {
-    setState({
-      ...state,
+    setCheckboxValues({
+      ...checkboxValues,
       [event.target.name]: event.target.checked,
     });
   };
-  const { children, dogs, cats, preApproval, terms } = state;
-  const error = [preApproval, terms].filter((v) => v).length !== 2;
+  const {
+    has_small_children,
+    smoker,
+    has_dogs,
+    has_cats,
+    wants_prepproval,
+    agrees_to_terms,
+    residence_owned,
+  } = checkboxValues;
+
+  const error = [wants_prepproval, agrees_to_terms].filter((v) => v).length < 1;
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <FormControl sx={{ m: 3 }} component='fieldset' variant='standard'>
-        <FormLabel component='legend'>Please select all that apply</FormLabel>
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Checkbox checked={dogs} onChange={handleChange} name='dogs' />
-            }
-            label='Dogs'
-          />
-          <FormControlLabel
-            control={
-              <Checkbox checked={cats} onChange={handleChange} name='cats' />
-            }
-            label='Cats'
-          />
-        </FormGroup>
-      </FormControl>
-      <FormControl sx={{ m: 3 }} component='fieldset' variant='standard'>
-        <FormLabel component='legend'>
-          <br></br>
-          <br></br>
-        </FormLabel>
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={children}
-                onChange={handleChange}
-                name='children'
-              />
-            }
-            label='Children'
-          />
+    <>
+      <Box sx={{ display: "flex", m: 3 }}>
+        <FormControl component='fieldset' variant='standard'>
+          <FormLabel component='legend'>Select all that apply:</FormLabel>
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={has_dogs}
+                  onChange={handleChange}
+                  name='has_dogs'
+                />
+              }
+              label='Own dogs'
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={has_cats}
+                  onChange={handleChange}
+                  name='has_cats'
+                />
+              }
+              label='Own cats'
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={smoker}
+                  onChange={handleChange}
+                  name='smoker'
+                  checkedIcon={<SmokingRoomsSharpIcon color='error' />}
+                  icon={<SmokeFreeSharpIcon color='success' />}
+                />
+              }
+              label='Smoker'
+            />
+          </FormGroup>
+        </FormControl>
+        <FormControl component='fieldset' variant='standard'>
+          <FormLabel component='legend'>
+            <br></br>
+          </FormLabel>
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={has_small_children}
+                  onChange={handleChange}
+                  name='has_small_children'
+                />
+              }
+              label='Have small children'
+            />
 
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={residence_owned}
+                  onChange={handleChange}
+                  name='residence_owned'
+                />
+              }
+              label='Residence Owned'
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={wants_prepproval}
+                  onChange={handleChange}
+                  name='wants_prepproval'
+                />
+              }
+              label='Pre-Approval?'
+            />
+          </FormGroup>
+        </FormControl>
+      </Box>
+      <Box sx={{ m: 3 }}>
+        <FormControl
+          required
+          error={error}
+          component='fieldset'
+          variant='standard'
+        >
+          <FormLabel>Required*</FormLabel>
           <FormControlLabel
             control={
               <Checkbox
-                checked={preApproval}
+                checkedIcon={<DoneOutlineSharpIcon color='success' />}
+                checked={agrees_to_terms}
                 onChange={handleChange}
-                name='preApproval'
+                name='agrees_to_terms'
               />
             }
-            label='Pre-Approval?'
+            label='I have read and accept the Terms and Conditions* *'
           />
-        </FormGroup>
-      </FormControl>
-      <FormControl
-        required
-        error={error}
-        component='fieldset'
-        sx={{ m: 3 }}
-        variant='standard'
-      >
-        <FormLabel>Required</FormLabel>
-        <br></br>
-        <FormControlLabel
-          control={
-            <Checkbox checked={terms} onChange={handleChange} name='terms' />
-          }
-          label='Terms and Conditions'
-        />
-      </FormControl>
-    </Box>
+        </FormControl>
+      </Box>
+    </>
   );
 }
