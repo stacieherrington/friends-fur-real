f47e7a78acc04bd92e944ba6f714e71db3948306
Tyler Male <postino.amichevole@gmail.com>
2022-10-19 08:12:23 -0400

    redux limited store included

--- ghi/src/index.js
+++ ghi/src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
