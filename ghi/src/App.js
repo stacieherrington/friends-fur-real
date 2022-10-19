50c73fe820c1d83d6e92d4c692385f4a57dd40d7
Tyler Male <postino.amichevole@gmail.com>
2022-10-19 09:04:14 -0400

    saving but coming back

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import DrawerAppBar from './DrawerAppBar';
-import ApplicationList from './applications/ApplicationsList';
-import HomePage from './Homepage';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import DrawerAppBar from "./DrawerAppBar";
+import ApplicationList from "./applications/ApplicationsList";
+import HomePage from "./Homepage";
 // import NavBar from './nav';
-import LoginForm from './Login/Login';
-import PetsList from './pets/PetsList';
-import Signup from './Signup/Signup';
-import PetForm from './pets/PetForm';
-import StoryForm from './components/StoryForm';
+import LoginForm from "./Login/Login";
+import PetsList from "./pets/PetsList";
+import Signup from "./Signup/Signup";
+import PetForm from "./pets/PetForm";
+import StoryForm from "./components/StoryForm";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <DrawerAppBar />
         {/* <NavBar /> */}
-        <div className="container py-5">
+        <div className='container py-5'>
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/applications" element={<ApplicationList />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/pets" element={<PetsList />} />
-            <Route path="/pets/create" element={<PetForm />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/stories/new" element={<StoryForm />} />
+            <Route path='/' element={<HomePage />} />
+            <Route path='/applications' element={<ApplicationList />} />
+            <Route path='/applications/new' element={<ApplicationList />} />
+            <Route path='/login' element={<LoginForm />} />
+            <Route path='/pets' element={<PetsList />} />
+            <Route path='/pets/create' element={<PetForm />} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/login' element={<LoginForm />} />
+            <Route path='/stories/new' element={<StoryForm />} />
           </Routes>
         </div>
       </BrowserRouter>
