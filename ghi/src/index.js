24e7fa6a392180d5fea0acb9eb4b7e71e1d9a016
Tyler Male <postino.amichevole@gmail.com>
2022-10-19 15:17:06 -0400

    eligible to merge

--- ghi/src/index.js
+++ ghi/src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { store } from './redux/store'
-import { Provider } from 'react-redux';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>
 );
-
