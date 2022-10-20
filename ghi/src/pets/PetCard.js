ab45f187b20e0c987d48dd4e60ff817154ac59a0
Tyler Male <postino.amichevole@gmail.com>
2022-10-20 19:51:04 -0400

    merging updated endpoints, but not all endpoints available

--- ghi/src/pets/PetCard.js
+++ ghi/src/pets/PetCard.js
@@ -1,17 +1,16 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
 import SliceAppForm from "../applications/SliceAppForm";
 export default function PetCard(props) {
-  
   const { id, rescue_id, pictures, name } = props;
   const [open, setOpen] = React.useState(false);
 
@@ -23,45 +22,55 @@ export default function PetCard(props) {
     setOpen(false);
   };
 
-
   return (
     <Card>
-      {props.pictures && props.pictures.length ? <CardMedia
-        component="img"
-        height="200"
-        image={props.pictures}
-        alt={props.breed}
-      /> : null}
+      {props.pictures && props.pictures.length ? (
+        <CardMedia
+          component='img'
+          height='200'
+          image={props.pictures}
+          alt={props.breed}
+        />
+      ) : null}
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+        <Typography gutterBottom variant='h5' component='div'>
           {props.name}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant='body2' color='text.secondary'>
           {props.description}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">More Info</Button>
-        <SliceAppForm pet_id={id} rescue_id={rescue_id}/>
+        <Button size='small'>More Info</Button>
+        <SliceAppForm pet_id={id} rescue_id={rescue_id} />
       </CardActions>
       <CardActions>
         {/* Only show this for staff/admin role */}
-        <Button size="small">Update</Button>
-        <Button size="small" onClick={handleClickOpen}>Delete</Button>
+        <Button size='small'>Update</Button>
+        <Button size='small' onClick={handleClickOpen}>
+          Delete
+        </Button>
         <Dialog
           open={open}
           onClose={handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
+          aria-labelledby='alert-dialog-title'
+          aria-describedby='alert-dialog-description'
         >
           <DialogContent>
-            <DialogContentText id="alert-dialog-description">
+            <DialogContentText id='alert-dialog-description'>
               Are you sure you want to delete this pet?
             </DialogContentText>
           </DialogContent>
           <DialogActions>
-            <Button onClick={handleClose} autoFocus>Cancel</Button>
-            <Button onClick={() => { handleClose(); props.handleDelete(props.id) }}>
+            <Button onClick={handleClose} autoFocus>
+              Cancel
+            </Button>
+            <Button
+              onClick={() => {
+                handleClose();
+                props.handleDelete(props.id);
+              }}
+            >
               Delete
             </Button>
           </DialogActions>
